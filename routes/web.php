cba5a8511b5ff167b773025202c4df4459882b0f
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-11-29 02:59:40 +0100

    add route on web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\TagController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +19,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::name('admin.')
+        ->prefix('admin')
+        ->group(function () {
+            Route::get('/', [DashboardController::class,'index'])->name('home');
+            Route::resource('posts', PostController::class);
+            Route::resource('categories', CategoryController::class);
+            Route::resource('tags', TagController::class);
+        });
