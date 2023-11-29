1fb8c69236e39c429aceea0fcb7d85b804e48787
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-11-29 02:34:38 +0100

    add category seeder

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use Illuminate\Support\Str;
 
 class CategoriesSeeder extends Seeder
 {
@@ -14,6 +16,15 @@ class CategoriesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = ['HTML', 'CSS', 'PHP', 'JavaScript', 'C++'];
+
+        foreach ($data as $category) {
+        $new_category = new Category();
+            $new_category->name = $category;
+            $new_category->slug = Str::slug($category, '-');
+            $new_category->save();
+        }
+
+
     }
 }
